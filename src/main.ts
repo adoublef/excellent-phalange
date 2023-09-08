be9b32bc83aa1dab08f066dbbfa61e497e5d0b08
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-08 22:02:20 +0100

    update elememnt to audio-sample

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import "./style.css";
-
 import { attr, controller, target } from "./catalyst";
 
+/* global context, we only allowed one anyway */
+const audioContext = new AudioContext();
+
 @controller
-export class HelloWorldElement extends HTMLElement {
+export class AudioSampleElement extends HTMLElement {
     @attr
-    name = "World";
+    src = "";
 
-    @target
-    declare display: HTMLSpanElement;
+    buffer: AudioBuffer | null;
 
-    connectedCallback() {
-        this.display.textContent = this.name;
+    async connectedCallback() {
+        const res = await fetch(this.src);
+        const arrayBuffer = await res.arrayBuffer();
+        this.buffer = await audioContext.decodeAudioData(arrayBuffer);
+    }
+
+    handleEvent() {
+        const { buffer } = this;
+        const sample = new AudioBufferSourceNode(audioContext, { buffer });
+        sample.connect(audioContext.destination);
+        sample.start(audioContext.currentTime);
     }
 }
