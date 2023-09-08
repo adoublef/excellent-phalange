2b0f6826c3bb3093359141ba9cd74afe272d870c
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-08 22:39:33 +0100

    use a provider to pass around audiocontext

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,42 @@
 import "./style.css";
-import { attr, controller, target } from "./catalyst";
+import { attr, consume, controller, providable, provide, target } from "./catalyst";
 
 /* global context, we only allowed one anyway */
-const audioContext = new AudioContext();
+// const baseContext = new AudioContext();
 
 @controller
+@providable
+export class AudioMixerElement extends HTMLElement {
+    @provide
+    baseContext = new AudioContext();
+}
+
+@controller
+@providable
 export class AudioSampleElement extends HTMLElement {
     @attr
     src = "";
+
+    @consume
+    baseContext: AudioContext
 
     buffer: AudioBuffer | null;
 
     async connectedCallback() {
         const res = await fetch(this.src);
         const arrayBuffer = await res.arrayBuffer();
-        this.buffer = await audioContext.decodeAudioData(arrayBuffer);
+        this.buffer = await this.baseContext.decodeAudioData(arrayBuffer);
     }
 
     handleEvent() {
-        const { buffer } = this;
-        const sample = new AudioBufferSourceNode(audioContext, { buffer });
-        sample.connect(audioContext.destination);
-        sample.start(audioContext.currentTime);
+        const { buffer, baseContext } = this;
+        const sample = new AudioBufferSourceNode(baseContext, { buffer });
+        sample.connect(baseContext.destination);
+        sample.start(baseContext.currentTime);
     }
 }
+
+@controller
+@providable
+export class EffectPluginElement extends HTMLElement { }
+
